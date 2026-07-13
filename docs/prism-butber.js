6fbd14cc2e3123191ddeac89f5e92fdf0bf37bed
Lebohang Mokoena <lebohang0824@gmail.com>
2026-07-13 07:47:58 +0200

    docs: add v1.13.0 changelog entry, update doc versions, add endpoint keywords to Prism highlighter

--- docs/prism-butber.js
+++ docs/prism-butber.js
@@ -28,6 +28,16 @@ Prism.languages.butter = {
       }
     }
   },
+  'endpoint-declaration': {
+    pattern: /\b(endpoint)\s+([A-Za-z_]\w*)/,
+    inside: {
+      'keyword': /endpoint/,
+      'function': {
+        pattern: /[A-Za-z_]\w*/,
+        alias: 'function'
+      }
+    }
+  },
   'param-declaration': {
     pattern: /\b(param)\s+([A-Za-z_]\w*)/,
     inside: {
@@ -38,6 +48,55 @@ Prism.languages.butter = {
       }
     }
   },
+  'response-declaration': {
+    pattern: /\b(response)\s+([A-Za-z_]\w*)/,
+    inside: {
+      'keyword': /response/,
+      'class-name': {
+        pattern: /[A-Za-z_]\w*/,
+        alias: 'class-name'
+      }
+    }
+  },
+  'field-declaration': {
+    pattern: /\b(field)\s+([A-Za-z_]\w*)/,
+    inside: {
+      'keyword': /field/,
+      'variable': {
+        pattern: /[A-Za-z_]\w*/,
+        alias: 'variable'
+      }
+    }
+  },
+  'return-statement': {
+    pattern: /\b(return)\s+(\d{3})\s+([A-Za-z_]\w*)/,
+    inside: {
+      'keyword': /return/,
+      'number': /\d{3}/,
+      'class-name': {
+        pattern: /[A-Za-z_]\w*/,
+        alias: 'class-name'
+      }
+    }
+  },
+  'return-string': {
+    pattern: /\b(return)\s+(\d{3})\s+("[^"]*")/,
+    inside: {
+      'keyword': /return/,
+      'number': /\d{3}/,
+      'string': {
+        pattern: /"[^"]*"/,
+        alias: 'string'
+      }
+    }
+  },
+  'return bare': {
+    pattern: /\b(return)\s+(\d{3})\b/,
+    inside: {
+      'keyword': /return/,
+      'number': /\d{3}/
+    }
+  },
   'conditional': /\b(?:if|unless|when|while)\b/,
-  'keyword': /\b(?:app|product|description|version|feature|params|param|type|required|default|validate|bool|boolean|length|enforce|actions|action)\b/
+  'keyword': /\b(?:app|product|description|version|feature|endpoint|params|param|type|required|default|validate|bool|boolean|length|enforce|actions|action|route|method|responses|response|field|returns|return)\b/
 };
